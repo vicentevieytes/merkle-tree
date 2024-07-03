6b8381569c0feaf1536bfc9126d878bcac0ea1c1
Vicente Vieytes <vicentevieytes@hotmail.com>
2024-07-03 17:28:24 -0300

    move auxiliary constructor funcions inside impl of MerkleTree and rename them to create_from and get_leaves

--- src/main.rs
+++ src/main.rs
@@ -44,59 +44,59 @@ struct MerkleTree {
 impl MerkleTree {
     fn new(data: &[u8]) -> Self {
         MerkleTree {
-            root: new_merkle_tree(data),
+            root: Self::create_from(data),
             data: data.to_vec(),
         }
     }
-}
 
-fn new_merkle_tree(data: &[u8]) -> MerkleNode {
-    // Construct the tree one level at a time.
-    // First level: constructing leaf MerkleNodes from data.
-    let mut current_level = merkle_leaves(&data);
+    fn create_from(data: &[u8]) -> MerkleNode {
+        // Construct the tree one level at a time.
+        // First level: constructing leaf MerkleNodes from data.
+        let mut current_level = Self::get_leaves(&data);
 
-    // Construct middle levels and finally root by combinig lower levels.
-    while current_level.len() > 1 {
-        current_level = next_merkle_level(current_level);
+        // Construct middle levels and finally root by combinig lower levels.
+        while current_level.len() > 1 {
+            current_level = Self::next_merkle_level(current_level);
+        }
+
+        return current_level[0].clone();
     }
 
-    return current_level[0].clone();
-}
-
-// Return even length vector of leaf MerkleNodes from &[u8]
-fn merkle_leaves(data: &[u8]) -> Vec<MerkleNode> {
-    let mut leaves: Vec<MerkleNode> = vec![];
-    for &block in data.iter() {
-        leaves.push(MerkleNode::new_leaf(&[block]));
-    }
-    // Leaves must be of even length
-    if leaves.len() % 2 == 1 {
-        let last_element = leaves[leaves.len() - 1].clone();
-        leaves.push(last_element);
-    }
-    return leaves;
-}
-
-// From a vec<MerkleNode>, iterate stepping by two
-// and create the next level by concatenating the hashes and hashing again
-fn next_merkle_level(current_level: Vec<MerkleNode>) -> Vec<MerkleNode> {
-    let mut next_level: Vec<MerkleNode> = vec![];
-    let mut i = 0;
-    while i < current_level.len() {
-        let left_node = &current_level[i];
-        let right_node = &current_level[i + 1];
-        let new_node = MerkleNode::combine(left_node, right_node);
-        next_level.push(new_node);
-        i += 2;
+    // Return even length vector of leaf MerkleNodes from &[u8]
+    fn get_leaves(data: &[u8]) -> Vec<MerkleNode> {
+        let mut leaves: Vec<MerkleNode> = vec![];
+        for &block in data.iter() {
+            leaves.push(MerkleNode::new_leaf(&[block]));
+        }
+        // Leaves must be of even length
+        if leaves.len() % 2 == 1 {
+            let last_element = leaves[leaves.len() - 1].clone();
+            leaves.push(last_element);
+        }
+        return leaves;
     }
 
-    // Make the next level of even length to repeat the process,
-    // only if it is not the root level.
-    if (next_level.len() != 1) && (next_level.len() % 2 == 1) {
-        let last_element = next_level[next_level.len() - 1].clone();
-        next_level.push(last_element);
+    // From a vec<MerkleNode>, iterate stepping by two
+    // and create the next level by concatenating the hashes and hashing again
+    fn next_merkle_level(current_level: Vec<MerkleNode>) -> Vec<MerkleNode> {
+        let mut next_level: Vec<MerkleNode> = vec![];
+        let mut i = 0;
+        while i < current_level.len() {
+            let left_node = &current_level[i];
+            let right_node = &current_level[i + 1];
+            let new_node = MerkleNode::combine(left_node, right_node);
+            next_level.push(new_node);
+            i += 2;
+        }
+
+        // Make the next level of even length to repeat the process,
+        // only if it is not the root level.
+        if (next_level.len() != 1) && (next_level.len() % 2 == 1) {
+            let last_element = next_level[next_level.len() - 1].clone();
+            next_level.push(last_element);
+        }
+        return next_level;
     }
-    return next_level;
 }
 fn main() {
     let merkle_tree = MerkleTree::new(&[1, 2, 3, 4, 5]);
