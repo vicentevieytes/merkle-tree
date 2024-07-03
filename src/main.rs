3374f96430aa9b1df4ccade599c62aaa9b5169c8
Vicente Vieytes <vicentevieytes@hotmail.com>
2024-07-03 03:14:14 -0300

    implement function hash_data to iterate over u8 slice and hash every element

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-extern crate crypto;
+extern crate sha2;
 
-use crypto::digest::Digest;
-use crypto::sha2::Sha256;
+use sha2::{Digest, Sha256};
+
+fn hash_data(data: &[u8]) {
+    let mut data_vector = vec![];
+    for &block in data.iter() {
+        let mut hasher = Sha256::new();
+        hasher.update(&[block]);
+        let hashed_data_block = hasher.finalize().to_vec();
+        data_vector.push(hashed_data_block);
+    }
+
+    println!("{:?}", data_vector[0].len())
+}
 
 fn main() {
-    println!("Hello, world!");
+    hash_data(&[1, 2, 3, 4])
 }
