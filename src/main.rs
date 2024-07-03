1ba73f522fa4601b89c0fb695e9c17c4b41a91cb
Vicente Vieytes <vicentevieytes@hotmail.com>
2024-07-03 11:15:01 -0300

    Add MerkleNode data struct and constructors
    MerkleNode will be a recursive data structure, for this effect, nodes
    left and right are wrapped in Option<Box<>>. Option is for the leaf
    nodes so that the don't have any children. Box is for the recursive
    structure.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,35 @@ extern crate sha2;
 
 use sha2::{Digest, Sha256};
 
-fn hash_data(data: &[u8]) {
+#[derive(Clone)]
+struct MerkleNode {
+    hash: Vec<u8>,
+    left: Option<Box<MerkleNode>>,
+    right: Option<Box<MerkleNode>>,
+}
+
+impl MerkleNode {
+    fn new(hash: Vec<u8>) -> Self {
+        MerkleNode {
+            hash,
+            left: None,
+            right: None,
+        }
+    }
+    fn combine(merkle_left: &MerkleNode, merkle_right: &MerkleNode) -> Self {
+        let mut hasher = Sha256::new();
+        hasher.update(&merkle_left.hash);
+        hasher.update(&merkle_right.hash);
+        let hash = hasher.finalize().to_vec();
+        MerkleNode {
+            hash,
+            left: Some(Box::new(merkle_left.clone())),
+            right: Some(Box::new(merkle_right.clone())),
+        }
+    }
+}
+
+fn hash_data_bytes(data: &[u8]) {
     let mut data_vector = vec![];
     for &block in data.iter() {
         let mut hasher = Sha256::new();
@@ -15,5 +43,5 @@ fn hash_data(data: &[u8]) {
 }
 
 fn main() {
-    hash_data(&[1, 2, 3, 4])
+    hash_data_bytes(&[1, 2, 3, 4])
 }
