22f283349786a83333a3f3a90c112f93af235142
Vicente Vieytes <vicentevieytes@hotmail.com>
2024-07-03 12:58:34 -0300

    Implement new_merkle_tree fully
    new_merkle_tree takes a &[u8], for each byte in the slice it creates a
    leaf node, then it constructs the higher levels until it reaches the
    Implement new_merkle_tree fully
    
    new_merkle_tree takes a &[u8], for each byte in the slice it creates a
    leaf node, then it constructs the higher levels until it reaches the
    root, which is a MerkleNode.root, which is a MerkleNode.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate sha2;
 
 use sha2::{Digest, Sha256};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct MerkleNode {
     hash: Vec<u8>,
     left: Option<Box<MerkleNode>>,
@@ -33,15 +33,41 @@ impl MerkleNode {
     }
 }
 
-fn new_merkle_tree(data: &[u8]) {
-    let mut data_vector = vec![];
+fn new_merkle_tree(data: &[u8]) -> MerkleNode {
+    // Construct the tree one level at a time.
+    // First level: constructing leaf MerkleNodes from data.
+    let mut current_level: Vec<MerkleNode> = vec![];
     for &block in data.iter() {
-        data_vector.push(MerkleNode::new_leaf(&[block]));
+        current_level.push(MerkleNode::new_leaf(&[block]));
     }
-    if data_vector.len() % 2 == 1 {
-        let last_element = data_vector[data_vector.len() - 1].clone();
-        data_vector.push(last_element);
+    if current_level.len() % 2 == 1 {
+        let last_element = current_level[current_level.len() - 1].clone();
+        current_level.push(last_element);
     }
+    // Construct middle levels and finally root by combinig lower levels.
+    let mut next_level: Vec<MerkleNode> = vec![];
+    while current_level.len() > 1 {
+        let mut i = 0;
+        while i < current_level.len() {
+            let left_node = &current_level[i];
+            let right_node = &current_level[i + 1];
+            let new_node = MerkleNode::combine(left_node, right_node);
+            next_level.push(new_node);
+            i += 2;
+        }
+        // Make the next level of even length to repeat the process,
+        // only if it is not the root level.
+        if (next_level.len() != 1) && (next_level.len() % 2 == 1) {
+            let last_element = next_level[next_level.len() - 1].clone();
+            next_level.push(last_element);
+        }
+        current_level = next_level.clone();
+        next_level = vec![];
+    }
+    return current_level[0].clone();
 }
 
-fn main() {}
+fn main() {
+    let merkle_tree = new_merkle_tree(&[1, 2, 3, 4, 5]);
+    println!("{:?}", merkle_tree.hash);
+}
