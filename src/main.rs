d2f23113ff7971eee16741974e743292d63fd5d3
Vicente Vieytes <vicentevieytes@hotmail.com>
2024-07-08 14:25:15 -0300

    Implement get_inclusion_proof function, write comments documenting funcitons

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use merkle_tree::merkle::MerkleTree;
 
 fn main() {
     let merkle_tree = MerkleTree::new(&[1, 2, 3, 4, 5]);
-    println!("{:?}", merkle_tree.get_root());
+    println!("{:?}", merkle_tree.get_root_node());
 }
